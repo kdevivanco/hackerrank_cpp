7c17f8f99b58f94f186133e3d767ebf595dafe57
kdevivanco <kdevivanco@gmail.com>
2023-04-26 11:40:57 -0500

    templateclass

---
+++ Template_Class/template_class_hackerrank.cpp
@@ -0,0 +1,68 @@
+//
+//KAyla de vivanco
+//
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+
+/*Write the class AddElements here*/
+template <class T>
+class AddElements{
+public:
+    vector<T> holder;
+    T sum;
+    AddElements(T element){
+        holder.push_back(element);
+    }
+
+    T add(T element){
+        sum = 0;
+        holder.push_back(element);
+        for (const auto& value : holder) {
+
+            sum += value;
+        }
+        return sum;
+    }
+
+    T concatenate(T element){
+        holder.push_back(element);
+        for (const auto& value : holder) {
+            sum += value;
+        }
+        return sum;
+    }
+};
+
+
+int main () {
+    int n,i;
+    cin >> n;
+    for(i=0;i<n;i++) {
+        string type;
+        cin >> type;
+        if(type=="float") {
+            double element1,element2;
+            cin >> element1 >> element2;
+            AddElements<double> myfloat (element1);
+            cout << myfloat.add(element2) << endl;
+        }
+        else if(type == "int") {
+            int element1, element2;
+            cin >> element1 >> element2;
+            AddElements<int> myint (element1);
+            cout << myint.add(element2) << endl;
+        }
+        else if(type == "string") {
+            string element1, element2;
+            cin >> element1 >> element2;
+            AddElements<string> mystring (element1);
+            cout << mystring.concatenate(element2) << endl;
+        }
+    }
+    return 0;
+}
